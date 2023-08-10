af25315fc57246f99e911056358d17f972df7f00
1x55 <k4i@pm.me>
2023-08-10 15:50:54 +0900

    completed Crypto Account  challenge

---
+++ crypto/js/main.js
@@ -0,0 +1,469 @@
+const WALLETS = [
+    {
+        wallet: "USD Wallet",
+        walletImg: "https://i.ibb.co/nLqdvY3/usd.png",
+        shortname: "USD",
+        transactions: [
+            {
+                transactionMonth: "Jun",
+                transactionDay: 12,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Jun",
+                transactionDay: 9,
+                transactionImg: "https://i.ibb.co/G0gwsdn/bitcoin-sold.png",
+                transactionAction: "Sold Bitcoin",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Jun",
+                transactionDay: 7,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "May",
+                transactionDay: 27,
+                transactionImg: "https://i.ibb.co/t2TrTvd/litecoin-bought.png",
+                transactionAction: "Bought Litecoin",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$25.97",
+                transactionTotalValue: "-$25.97"
+            },
+            {
+                transactionMonth: "May",
+                transactionDay: 22,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "May",
+                transactionDay: 5,
+                transactionImg: "https://i.ibb.co/G0gwsdn/bitcoin-sold.png",
+                transactionAction: "Sold Bitcoin",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Apr",
+                transactionDay: 19,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Apr",
+                transactionDay: 4,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Apr",
+                transactionDay: 1,
+                transactionImg: "https://i.ibb.co/h8zKXH2/bitcoin-cash-sold.png",
+                transactionAction: "Sold Bitcoin Cash",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Mar",
+                transactionDay: 12,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Mar",
+                transactionDay: 8,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Feb",
+                transactionDay: 28,
+                transactionImg: "https://i.ibb.co/LCpLZYM/litecoin-sold.png",
+                transactionAction: "Sold Litecoin",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$21.37",
+                transactionTotalValue: "-$21.37"
+            },
+            {
+                transactionMonth: "Feb",
+                transactionDay: 22,
+                transactionImg: "https://i.ibb.co/hHX7xgS/bitcoin-cash-bought.png",
+                transactionAction: "Bought Bitcoin Cash",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$100.83",
+                transactionTotalValue: "-$100.83"
+            },
+            {
+                transactionMonth: "Jan",
+                transactionDay: 13,
+                transactionImg: "https://i.ibb.co/dJYKcG0/bitcoin-bought.png",
+                transactionAction: "Bought Bitcoin",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$222.44",
+                transactionTotalValue: "-$222.44"
+            },
+        ]
+    },
+    {
+        wallet: "BTC Wallet",
+        walletImg: "https://i.ibb.co/3B8WLhG/btc.png",
+        shortname: "BTC",
+        transactions: [
+            {
+                transactionMonth: "Jun",
+                transactionDay: 9,
+                transactionImg: "https://i.ibb.co/G0gwsdn/bitcoin-sold.png",
+                transactionAction: "Sold Bitcoin",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "May",
+                transactionDay: 5,
+                transactionImg: "https://i.ibb.co/G0gwsdn/bitcoin-sold.png",
+                transactionAction: "Sold Bitcoin",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Jan",
+                transactionDay: 13,
+                transactionImg: "https://i.ibb.co/dJYKcG0/bitcoin-bought.png",
+                transactionAction: "Bought Bitcoin",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$222.44",
+                transactionTotalValue: "-$222.44"
+            },
+        ]
+    },
+    {
+        wallet: "BCH Wallet",
+        walletImg: "https://i.ibb.co/hMytkV9/bch.png",
+        shortname: "BCH",
+        transactions: [
+            {
+                transactionMonth: "Apr",
+                transactionDay: 1,
+                transactionImg: "https://i.ibb.co/h8zKXH2/bitcoin-cash-sold.png",
+                transactionAction: "Sold Bitcoin Cash",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Feb",
+                transactionDay: 22,
+                transactionImg: "https://i.ibb.co/hHX7xgS/bitcoin-cash-bought.png",
+                transactionAction: "Bought Bitcoin Cash",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$100.83",
+                transactionTotalValue: "-$100.83"
+            },
+        ]
+    },
+    {
+        wallet: "ETH Wallet",
+        walletImg: "https://i.ibb.co/GC22mTt/eth.png",
+        shortname: "ETH",
+        transactions: [
+            {
+                transactionMonth: "Jun",
+                transactionDay: 12,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Jun",
+                transactionDay: 7,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "May",
+                transactionDay: 22,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Apr",
+                transactionDay: 19,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Apr",
+                transactionDay: 4,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Mar",
+                transactionDay: 12,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+            {
+                transactionMonth: "Mar",
+                transactionDay: 8,
+                transactionImg: "https://i.ibb.co/jJ6z6Md/ethereum-bought.png",
+                transactionAction: "Bought Ethereum",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$6.83",
+                transactionTotalValue: "-$6.83"
+            },
+        ]
+    },
+    {
+        wallet: "LTC Wallet",
+        walletImg: "https://i.ibb.co/Ky3fq4k/lc.png",
+        shortname: "LTC",
+        transactions: [
+            {
+                transactionMonth: "May",
+                transactionDay: 27,
+                transactionImg: "https://i.ibb.co/t2TrTvd/litecoin-bought.png",
+                transactionAction: "Bought Litecoin",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$25.97",
+                transactionTotalValue: "-$25.97"
+            },
+            {
+                transactionMonth: "Feb",
+                transactionDay: 28,
+                transactionImg: "https://i.ibb.co/LCpLZYM/litecoin-sold.png",
+                transactionAction: "Sold Litecoin",
+                transactionUsingWith: "Using USD Wallet",
+                transactionValue: "-$21.37",
+                transactionTotalValue: "-$21.37"
+            },
+        ]
+    }
+];
+
+const walletsListContainer = document.querySelector('.wallets_list');
+const transactionsListContainer = document.querySelector('.transactions_list');
+const newAccountBtn = document.querySelector('.new_account_btn');
+const accountCreator = document.querySelector('.account_creator');
+const overlay = document.querySelector('.overlay');
+let wallets = document.querySelectorAll('.wallet');
+
+const openAccountCreator = () => {
+    accountCreator.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
+    const createAccountBtn = document.querySelector('.create_btn');
+    const closeBtn = document.querySelector('.close_btn');
+
+    createAccountBtn.addEventListener('click', e => {
+        e.preventDefault();
+        validateForm();
+    });
+    closeBtn.addEventListener('click', e => {
+        e.preventDefault();
+        closeAccountCreator();
+    });
+}
+
+const closeAccountCreator = () => {
+    accountCreator.classList.add('hidden');
+    overlay.classList.add('hidden');
+    const allErrors = document.querySelectorAll('.error');
+    allErrors.forEach(err => {
+        err.classList.add('hidden');
+        err.textContent = '';
+    });
+}
+
+//validation
+const validateForm = () => {
+    let errors = 0;
+    const walletNameInp = document.querySelector('#new_wallet_name');
+    const wallettShortnameInp = document.querySelector('#new_wallet_shortname');
+
+    const walletImgInp = document.querySelector('#new_wallet_img'); 
+
+    if (walletNameInp.value.length < 5) {
+        showError(walletNameInp, "Name must contain at least 5 characters");
+        errors++
+    } else {
+        hideError(walletNameInp);
+    }
+
+    if (walletShortnameInp.value.length !== 3) {
+        showError(walletShortnameInp, "Shortname must contain exactly 3 characters");
+        errors++;
+    } else {
+        hideError(walletShortnameInp);
+    }
+
+
+    if (!walletImgInp.value.length) {
+        showError(walletImgInp, "This field cannot be empty");
+        errors++
+    } else {
+        hideError(walletImgInp);
+    }
+
+    if (errors === 0) {
+        closeAccountCreator();
+        addNewAccount({
+            wallet: walletNameInp.value,
+            walletImg: walletImgInp.value,
+            shortname: walletShortnameInp.value.toUpperCase(),
+            transactions: []
+        })
+        walletNameInp.value = '';
+        walletShortnameInp.value = '';
+        walletImgInp.value = '';
+    }
+}
+
+const showError = (input, msg) => {
+    const errorEl = input.parentElement.querySelector('.error');
+    errorEl.classList.remove('hidden');
+    errorEl.textContent = msg;
+}
+const hideError = input => {
+    const errorEl = input.parentElement.querySelector('.error');
+    errorEl.classList.add('hidden');
+}
+
+const addNewAccount = obj => {
+    WALLETS.push(obj);
+    createNewAccountElement(obj);
+}
+
+const createNewAccountElement = obj => {
+    const walletEl = document.createElement('div');
+    walletEl.className = "wallet";
+    let walletElHTMLTemplate = `
+    <img class="wallet_img" src="${obj.walletImg}" alt="">
+    <div class="wallet_info">
+          <span>${obj.wallet}</span>
+          <span>0.0000 ${obj.shortname} ≈ <span>$0.00</span></span>
+          <div class="wallet_actions">
+             <button class="send_btn inactive">
+                <img src="https://i.ibb.co/FXhrBtw/send.png" alt="">
+                Send
+           </button>
+               <button class="receive_btn">
+               <img src="https://i.ibb.co/dKMPr6S/receive.png" alt="">
+                Receive
+            </button>
+         </div>
+      </div>
+    `;
+    walletsListContainer.appendChild(walletEl);
+    walletEl.insertAdjacentHTML('beforeend', walletElHTMLTemplate);
+    wallets = document.querySelectorAll('.wallet');
+    walletEl.addEventListener('click', () => {
+        selectWallet(walletEl);
+        renderTransactions(walletEl.querySelector('.wallet_info span:nth-child(1)').textContent);
+    });
+}
+
+const renderTransactions = wallet => {
+    transactionsListContainer.classList.add('loading');
+    setTimeout(() => {
+        transactionsListContainer.classList.remove('loading');
+    }, 300);
+    transactionsListContainer.innerHTML = '';
+    const transactionsForWallet = WALLETS.filter(x => x.wallet === wallet);
+
+    if (transactionsForWallet[0].transactions.length === 0) {
+        let noTransactionsInfoHTML = `
+            <p class="no_transactions_info">You have no transactions for the ${wallet}</p>
+        `;
+        transactionsListContainer.insertAdjacentHTML('beforeend', noTransactionsInfoHTML);
+    }
+
+    transactionsForWallet[0].transactions.forEach(transaction => {
+        let transactionHTMLTemplate = `
+             <div class="transaction">
+                <div class="transaction_date">
+                    <span>${transaction.transactionMonth}</span>
+                    <span>${transaction.transactionDay}</span>
+                </div>
+                <img src="${transaction.transactionImg}" alt="">
+                <div class="transaction_info">
+                    <span>${transaction.transactionAction}</span>
+                    <span>${transaction.transactionUsingWith}</span>
+                </div>
+                <div class="transaction_value">
+                    <span>${transaction.transactionValue}</span>
+                    <span>${transaction.transactionTotalValue}</span>
+                </div>
+            </div>
+        `;
+
+        transactionsListContainer.insertAdjacentHTML('beforeend', transactionHTMLTemplate);
+    });
+}
+
+const selectWallet = wallet => {
+    wallets.forEach(wallet => {
+        wallet.classList.remove('selected');
+    });
+    wallet.classList.add('selected');
+}
+
+wallets.forEach(wallet => {
+    wallet.addEventListener('click', () => {
+        selectWallet(wallet);
+        renderTransactions(wallet.querySelector('.wallet_info span:nth-child(1)').textContent);
+    });
+});
+
+newAccountBtn.addEventListener('click', openAccountCreator);
+
+window.addEventListener('DOMContentLoaded', () => {
+    renderTransactions("USD Wallet");
+});
